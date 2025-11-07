767e53b15063c6404bb3d39a049117f180f0715b
milenaossorio <milena.ossorio@gmail.com>
2025-11-07 11:37:43 +0100

    ghappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,37 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    private GHappy ghappy;
+
+    @BeforeEach
+    public void initialize() {
+        this.ghappy = new GHappy();
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "gg", "asdfgg", "ggasfa", "dsgggsdsdf", "asdfggggdasfggsdf" })
+    public void isHappy(String s) {
+        boolean result = ghappy.gHappy(s);
+        Assertions.assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "g", "asdfg", "gasfa", "dsggagsdsdf", "asdfgkgggdasfggsdf" })
+    public void isNotHappy(String s) {
+        boolean result = ghappy.gHappy(s);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void isHappy1() {
+        boolean result = ghappy.gHappy("");
+        Assertions.assertTrue(result);
+    }
 }
